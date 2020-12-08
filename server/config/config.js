c2721ea5857c90d29adcd215ad80da7d00782ac2
Martin Ober <martinober@MacBook-Air-de-Martin.local>
2020-12-08 14:28:53 -0300

    Restserver - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,22 @@
+// =============================
 // PUERTO
+// =============================
 process.env.PORT = process.env.PORT || 3000;
+
+// =============================
+// ENTORNO
+// =============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// =============================
+// BASE DE DATOS
+// =============================
+let urlBD;
+
+if(process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe';
+} else {
+    urlBD = 'mongodb+srv://martin:mW6dlTunDnkgKAjA@cluster0.airxi.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlBD;
