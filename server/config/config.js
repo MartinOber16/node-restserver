ba8874c0461f233d6184fae790f6645a13e8b150
Martin Ober <martinober@MacBook-Air-de-Martin.local>
2020-12-08 14:51:06 -0300

    -Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlBD;
 if(process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://martin:mW6dlTunDnkgKAjA@cluster0.airxi.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlBD;
